b8d4de424660d369481bd5e3c2eb204ad35074f0
Daniel Marinho <danielmarinho8@hotmail.com>
2024-09-24 13:14:57 -0300

    [hotfix][feature] - header shadow

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,6 +14,7 @@ export default function Header() {
                 alignItems: 'center',
                 borderBottomRightRadius: '3.5rem',
                 borderBottomLeftRadius: '3.5rem',
+                boxShadow: '4px 4px 8px rgba(0, 0, 0, 0.3)',
             }}
         >
             <WaterIcon sx={{
