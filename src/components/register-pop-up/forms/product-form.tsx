bb0cc5a5806b2d090418719759cc75826f526981
rafael.santos <rafaelsantos32141@gmail.com>
2024-10-23 14:59:54 -0300

    Implementação do component separado de form de criação/edição de vendas e produtos

---
+++ src/components/register-pop-up/forms/product-form.tsx
@@ -0,0 +1,216 @@
+import { IconButton, TextField, Tooltip } from "@mui/material";
+import { FormEvent, useRef, useState } from "react";
+import { AlertInfo } from "../../custom-alert";
+import { blobToString, getTrimmed, resizeImage } from "../../../utils";
+import { useGlobalState } from "../../../global-state-context";
+import Button from "../../button/button";
+import {
+  editData,
+  generateDocId,
+  insertData,
+  uploadFile,
+} from "../../../apis/firebase";
+import { Product } from "../../../types";
+import { CancelOutlined, FileUploadOutlined } from "@mui/icons-material";
+
+type Props = {
+  isWaitingAsync: boolean;
+  setIsWaitingAsync: React.Dispatch<React.SetStateAction<boolean>>;
+  setAlertInfo: React.Dispatch<React.SetStateAction<AlertInfo | undefined>>;
+  close: () => void;
+  editingData?: Product;
+};
+
+type RegisterProductType = {
+  name: string;
+  price: string;
+  photoSrc?: string;
+};
+
+function getInitialData(editingData?: Product): RegisterProductType {
+  const data: RegisterProductType = {
+    name: editingData?.name || "",
+    price: editingData?.price.toFixed(2).replace(".", ",") || "",
+  };
+  if (!!editingData?.photoSrc) data.photoSrc = editingData.photoSrc;
+
+  return data;
+}
+
+export default function ProductForm({
+  isWaitingAsync,
+  setIsWaitingAsync,
+  setAlertInfo,
+  close,
+  editingData,
+}: Props) {
+  const globalState = useGlobalState();
+  const isEditing = !!editingData;
+
+  const [data, setData] = useState(getInitialData(editingData));
+
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  async function handleImageChange(e: React.ChangeEvent<HTMLInputElement>) {
+    if (isWaitingAsync) return;
+
+    if (e.target.files && e.target.files[0]) {
+      const image = e.target.files[0];
+      setIsWaitingAsync(true);
+      try {
+        const resizedImage = await resizeImage(image, 300, 300);
+        setData({ ...data, photoSrc: await blobToString(resizedImage) });
+      } catch (e) {
+        console.log(e);
+        setAlertInfo({
+          severity: "error",
+          message: "Falha ao tentar carregar imagem",
+        });
+      } finally {
+        setIsWaitingAsync(false);
+        e.target.value = "";
+      }
+    }
+  }
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (isWaitingAsync || !globalState) return;
+
+    try {
+      setIsWaitingAsync(true);
+      const trimmedData = getTrimmed(data);
+      setData({ ...trimmedData });
+
+      let id: string | undefined = undefined;
+      const finalData: any = { ...trimmedData };
+      finalData.price = Number(finalData.price.replace(",", "."));
+
+      if (isNaN(finalData.price) || finalData.price <= 0)
+        return setAlertInfo({
+          severity: "error",
+          message: `Preço inválido`,
+        });
+
+      finalData.photoSrc = finalData.photoSrc || "";
+      if (isEditing) {
+        if (editingData.photoSrc === finalData.photoSrc) {
+          delete finalData.photoSrc;
+        } else if (!!finalData.photoSrc) {
+          const response = await fetch(finalData.photoSrc);
+          const blob = await response.blob();
+          finalData.photoSrc = await uploadFile(
+            editingData.id,
+            "photo.png",
+            blob
+          );
+        }
+      } else if (!!finalData.photoSrc) {
+        id = generateDocId("products");
+        const response = await fetch(finalData.photoSrc);
+        const blob = await response.blob();
+        finalData.photoSrc = await uploadFile(id, "photo.png", blob);
+      }
+
+      let err = "";
+      if (isEditing) {
+        err = await editData("products", editingData.id, finalData);
+      } else {
+        err = await insertData("products", finalData);
+      }
+
+      if (!err) {
+        setAlertInfo({
+          severity: "success",
+          message: `Produto ${isEditing ? "editado" : "cadastrado"}`,
+        });
+        close();
+      } else {
+        setAlertInfo({
+          severity: "error",
+          message: err,
+        });
+      }
+    } catch (e) {
+      console.log(e);
+      setAlertInfo({
+        severity: "error",
+        message: `Falha ao tentar ${
+          isEditing ? "editar" : "cadastrar"
+        } produto`,
+      });
+    } finally {
+      setIsWaitingAsync(false);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        ref={fileInputRef}
+        onChange={handleImageChange}
+        style={{ display: "none" }}
+        type="file"
+        accept="image/*"
+      />
+
+      <div className="two-fields-container">
+        <TextField
+          label="Nome"
+          variant="outlined"
+          type="text"
+          required
+          value={data.name}
+          onChange={(e) => setData({ ...data, name: e.target.value })}
+        />
+        <TextField
+          label="Preço"
+          variant="outlined"
+          type="text"
+          inputProps={{
+            pattern: "^[0-9]+(,[0-9]{1,2})?$",
+            title: "O CEP deve possuir 8 dígitos",
+          }}
+          required
+          value={data.price}
+          onChange={(e) => {
+            const value = e.target.value.trim().replace(".", ",");
+            const priceRegex = /^\d+(,\d{0,2})?$/;
+            if (value === "" || priceRegex.test(value))
+              setData({ ...data, price: value });
+          }}
+        />
+      </div>
+
+      <div className="file-input-container">
+        <IconButton
+          className="clear-btn"
+          color="error"
+          disabled={isWaitingAsync || !data.photoSrc}
+          onClick={() => {
+            delete data.photoSrc;
+            setData({ ...data });
+          }}
+        >
+          <CancelOutlined />
+        </IconButton>
+        {!!data.photoSrc ? (
+          <img src={data.photoSrc} draggable={false} alt="Foto do produto" />
+        ) : (
+          <Tooltip title="Fazer upload de foto">
+            <Button
+              onClick={() => fileInputRef.current?.click()}
+              disabled={isWaitingAsync}
+            >
+              <FileUploadOutlined />
+            </Button>
+          </Tooltip>
+        )}
+      </div>
+
+      <Button type="submit" disabled={isWaitingAsync}>
+        {isEditing ? "Editar" : "Cadastrar"}
+      </Button>
+    </form>
+  );
+}
---
+++ src/components/register-pop-up/forms/sale-form.tsx
@@ -0,0 +1,528 @@
+import {
+  Card,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import { FormEvent, useState } from "react";
+import { AlertInfo } from "../../custom-alert";
+import {
+  formatVehicle,
+  getSaleValue,
+  getTrimmed,
+  roundNumber,
+} from "../../../utils";
+import { useGlobalState } from "../../../global-state-context";
+import Button from "../../button/button";
+import { editData, insertData } from "../../../apis/firebase";
+import { CollaboratorType, Sale } from "../../../types";
+import { Divider } from "../register-pop-up";
+import Logo from "../../../assets/logo.png";
+import {
+  AddOutlined,
+  ImageNotSupportedOutlined,
+  RemoveOutlined,
+} from "@mui/icons-material";
+
+type Props = {
+  isWaitingAsync: boolean;
+  setIsWaitingAsync: React.Dispatch<React.SetStateAction<boolean>>;
+  setAlertInfo: React.Dispatch<React.SetStateAction<AlertInfo | undefined>>;
+  close: () => void;
+  editingData?: Sale;
+};
+
+export default function SaleForm({
+  isWaitingAsync,
+  setIsWaitingAsync,
+  setAlertInfo,
+  close,
+  editingData,
+}: Props) {
+  const globalState = useGlobalState();
+  const isEditing = !!editingData;
+
+  const isAdmin = globalState?.loggedUser.type === CollaboratorType.Admin;
+
+  const [data, setData] = useState({
+    collaboratorId: isAdmin ? "" : globalState?.loggedUser.id || "",
+    vehicleId: "",
+    paymentMethodId: "",
+    client: isEditing
+      ? {
+          name: "",
+          phone: "",
+          cep: "",
+          city: "",
+          neighborhood: "",
+          street: "",
+          number: "",
+          complement: "",
+        }
+      : "",
+    products: editingData?.products || {},
+    paidFullPrice: !isEditing,
+    paidValue: editingData?.paidValue.toFixed(2).replace(".", ",") || "",
+  });
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (isWaitingAsync || !globalState) return;
+
+    try {
+      setIsWaitingAsync(true);
+      const trimmedData = getTrimmed(data);
+      setData({ ...trimmedData });
+
+      let finalData: any = { ...trimmedData };
+      if (Object.keys(finalData.products).length === 0) {
+        return setAlertInfo({
+          severity: "error",
+          message: "Nenhum produto selecionado",
+        });
+      }
+
+      const saleValue = getSaleValue(finalData.products);
+      if (finalData.paidFullPrice) {
+        finalData.paidValue = saleValue;
+      } else {
+        finalData.paidValue = roundNumber(
+          Number(finalData.paidValue.replace(",", "."))
+        );
+        if (
+          isNaN(finalData.paidValue) ||
+          finalData.paidValue < 0 ||
+          finalData.paidValue > saleValue
+        ) {
+          return setAlertInfo({
+            severity: "error",
+            message: "Valor pago inválido",
+          });
+        }
+      }
+
+      delete finalData.paidFullPrice;
+      if (isEditing) {
+        finalData = { paidValue: finalData.paidValue };
+      }
+
+      let err = "";
+      if (isEditing) {
+        err = await editData("sales", editingData.id, finalData);
+      } else {
+        err = await insertData("sales", finalData);
+      }
+
+      if (!err) {
+        setAlertInfo({
+          severity: "success",
+          message: `Venda ${isEditing ? "editada" : "cadastrada"}`,
+        });
+        close();
+      } else {
+        setAlertInfo({
+          severity: "error",
+          message: err,
+        });
+      }
+    } catch (e) {
+      console.log(e);
+      setAlertInfo({
+        severity: "error",
+        message: `Falha ao tentar ${isEditing ? "editar" : "cadastrar"} venda`,
+      });
+    } finally {
+      setIsWaitingAsync(false);
+    }
+  }
+
+  if (!globalState) return null;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {!isEditing && (
+        <>
+          <Divider text="Informações da venda" />
+
+          <div
+            className={
+              isAdmin ? "three-fields-container" : "two-fields-container"
+            }
+          >
+            {isAdmin && (
+              <FormControl required>
+                <InputLabel id="collaborator-select-label">
+                  Funcionário
+                </InputLabel>
+                <Select
+                  labelId="collaborator-select-label"
+                  label="Funcionário"
+                  value={data.collaboratorId}
+                  onChange={(e) =>
+                    setData({ ...data, collaboratorId: e.target.value })
+                  }
+                >
+                  {globalState.collaborators.map((c) => (
+                    <MenuItem
+                      key={c.id}
+                      value={c.id}
+                    >{`${c.name} - ${c.email}`}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            )}
+            <FormControl required>
+              <InputLabel id="vehicle-select-label">Veículo</InputLabel>
+              <Select
+                labelId="vehicle-select-label"
+                label="Veículo"
+                value={data.vehicleId}
+                onChange={(e) =>
+                  setData({ ...data, vehicleId: e.target.value })
+                }
+              >
+                {globalState.vehicles.map((v) => (
+                  <MenuItem key={v.id} value={v.id}>
+                    {formatVehicle(v)}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <FormControl required>
+              <InputLabel id="payment-method-select-label">
+                Mét. de pagamento
+              </InputLabel>
+              <Select
+                labelId="payment-method-select-label"
+                label="Mét. de pagamento"
+                value={data.paymentMethodId}
+                onChange={(e) =>
+                  setData({ ...data, paymentMethodId: e.target.value })
+                }
+              >
+                {globalState.paymentMethods.map((p) => (
+                  <MenuItem key={p.id} value={p.id}>
+                    {p.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+
+          <Divider text="Cliente" />
+
+          <div className="two-fields-container">
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={typeof data.client === "string"}
+                    onChange={(e) => {
+                      const { checked } = e.target;
+                      setData({
+                        ...data,
+                        client: checked
+                          ? ""
+                          : {
+                              name: "",
+                              phone: "",
+                              cep: "",
+                              city: "",
+                              neighborhood: "",
+                              street: "",
+                              number: "",
+                              complement: "",
+                            },
+                      });
+                    }}
+                  />
+                }
+                label="Usar cliente cadastrado"
+                labelPlacement="start"
+              />
+            </FormGroup>
+            <FormControl disabled={typeof data.client !== "string"} required>
+              <InputLabel id="client-select-label">Cliente</InputLabel>
+              <Select
+                labelId="client-select-label"
+                label="Cliente"
+                value={typeof data.client === "string" ? data.client : ""}
+                onChange={(e) => setData({ ...data, client: e.target.value })}
+              >
+                {globalState.clients.map((c) => (
+                  <MenuItem key={c.id} value={c.id}>{`${c.name}${
+                    !!c.cpfCnpj ? ` - ${c.cpfCnpj}` : ""
+                  }`}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+
+          {typeof data.client === "object" && (
+            <>
+              <div className="two-fields-container">
+                <TextField
+                  label="Nome"
+                  variant="outlined"
+                  type="text"
+                  required
+                  value={data.client.name}
+                  onChange={(e) => {
+                    (data.client as any).name = e.target.value;
+                    setData({ ...data });
+                  }}
+                />
+                <TextField
+                  label="Telefone"
+                  variant="outlined"
+                  type="text"
+                  inputProps={{
+                    pattern: "^[0-9]{10,11}$",
+                    title: "O telefone deve conter 10 ou 11 dígitos",
+                    maxLength: 11,
+                  }}
+                  required
+                  value={data.client.phone}
+                  onChange={(e) => {
+                    let { value } = e.target;
+                    value = value.replace(/[^0-9]/g, "");
+                    (data.client as any).phone = value;
+                    setData({ ...data });
+                  }}
+                />
+              </div>
+              <div className="three-fields-container">
+                <TextField
+                  label="CEP"
+                  variant="outlined"
+                  type="text"
+                  inputProps={{
+                    pattern: "^[0-9]{8}$",
+                    title: "O CEP deve possuir 8 dígitos",
+                    maxLength: 8,
+                  }}
+                  value={data.client.cep}
+                  onChange={(e) => {
+                    let { value } = e.target;
+                    value = value.replace(/[^0-9]/g, "");
+                    (data.client as any).cep = value;
+                    setData({ ...data });
+                  }}
+                />
+                <TextField
+                  label="Cidade"
+                  variant="outlined"
+                  type="text"
+                  required
+                  value={data.client.city}
+                  onChange={(e) => {
+                    (data.client as any).city = e.target.value;
+                    setData({ ...data });
+                  }}
+                />
+                <TextField
+                  label="Bairro"
+                  variant="outlined"
+                  type="text"
+                  required
+                  value={data.client.neighborhood}
+                  onChange={(e) => {
+                    (data.client as any).neighborhood = e.target.value;
+                    setData({ ...data });
+                  }}
+                />
+              </div>
+              <div className="three-fields-container">
+                <TextField
+                  label="Logradouro"
+                  variant="outlined"
+                  type="text"
+                  required
+                  value={data.client.street}
+                  onChange={(e) => {
+                    (data.client as any).street = e.target.value;
+                    setData({ ...data });
+                  }}
+                />
+                <TextField
+                  label="Número"
+                  variant="outlined"
+                  type="text"
+                  value={data.client.number}
+                  onChange={(e) => {
+                    let { value } = e.target;
+                    value = value.replace(/[^0-9]/g, "");
+                    (data.client as any).number = value;
+                    setData({ ...data });
+                  }}
+                />
+                <TextField
+                  label="Complemento"
+                  variant="outlined"
+                  type="text"
+                  value={data.client.complement}
+                  onChange={(e) => {
+                    (data.client as any).complement = e.target.value;
+                    setData({ ...data });
+                  }}
+                />
+              </div>
+            </>
+          )}
+
+          <Divider text="Produtos" />
+
+          <div className="products-container">
+            {globalState.products.map((p) => (
+              <Card key={p.id}>
+                {!!p.photoSrc ? (
+                  <img
+                    className="product-img"
+                    src={p.photoSrc || Logo}
+                    alt={p.name}
+                    draggable={false}
+                  />
+                ) : (
+                  <div className="product-img">
+                    <ImageNotSupportedOutlined fontSize="large" />
+                  </div>
+                )}
+
+                <span>{p.name}</span>
+                <span>{`R$ ${p.price.toFixed(2).replace(".", ",")}`}</span>
+
+                <div className="set-quantity-container">
+                  <IconButton
+                    color="primary"
+                    disabled={isWaitingAsync || !data.products[p.id]?.quantity}
+                    onClick={() => {
+                      if (p.id in data.products) {
+                        data.products[p.id].quantity--;
+                        if (data.products[p.id].quantity === 0) {
+                          delete data.products[p.id];
+                        }
+
+                        setData({ ...data });
+                      }
+                    }}
+                  >
+                    <RemoveOutlined />
+                  </IconButton>
+                  <span>{data.products[p.id]?.quantity || "0"}</span>
+                  <IconButton
+                    color="primary"
+                    onClick={() => {
+                      if (p.id in data.products) {
+                        data.products[p.id].quantity++;
+                      } else {
+                        data.products[p.id] = {
+                          price: p.price,
+                          quantity: 1,
+                        };
+                      }
+
+                      setData({ ...data });
+                    }}
+                  >
+                    <AddOutlined />
+                  </IconButton>
+                </div>
+              </Card>
+            ))}
+          </div>
+
+          <Divider
+            text={`Pagamento (Total: R$ ${getSaleValue(data.products, true)})`}
+          />
+
+          <div className="two-fields-container">
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={data.paidFullPrice}
+                    onChange={(e) => {
+                      const { checked } = e.target;
+                      let paidValue = checked ? "" : "0";
+
+                      setData({
+                        ...data,
+                        paidFullPrice: checked,
+                        paidValue,
+                      });
+                    }}
+                  />
+                }
+                label="Pagou valor total"
+                labelPlacement="start"
+              />
+            </FormGroup>
+            <TextField
+              label="Valor pago"
+              variant="outlined"
+              type="text"
+              required
+              disabled={data.paidFullPrice}
+              value={data.paidValue}
+              onChange={(e) => {
+                const { value } = e.target;
+                if (/^(\d+(,\d{0,2})?)?$/.test(value)) {
+                  setData({ ...data, paidValue: value });
+                }
+              }}
+            />
+          </div>
+        </>
+      )}
+      {isEditing && (
+        <div className="two-fields-container">
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={data.paidFullPrice}
+                  onChange={(e) => {
+                    const { checked } = e.target;
+                    let paidValue = checked ? "" : "0";
+
+                    setData({
+                      ...data,
+                      paidFullPrice: checked,
+                      paidValue,
+                    });
+                  }}
+                />
+              }
+              label="Pagou valor total"
+              labelPlacement="start"
+            />
+          </FormGroup>
+          <TextField
+            label="Valor pago"
+            variant="outlined"
+            type="text"
+            required
+            disabled={data.paidFullPrice}
+            value={data.paidValue}
+            onChange={(e) => {
+              const { value } = e.target;
+              if (/^(\d+(,\d{0,2})?)?$/.test(value)) {
+                setData({ ...data, paidValue: value });
+              }
+            }}
+          />
+        </div>
+      )}
+
+      <Button type="submit" disabled={isWaitingAsync}>
+        {isEditing ? "Editar" : "Cadastrar"}
+      </Button>
+    </form>
+  );
+}
